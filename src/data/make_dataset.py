965cffd1827c7319ec90922280e0abb6309476a4
AdnanSevincDS <72599641+AdnanSevincDS@users.noreply.github.com>
2024-03-18 11:19:35 +0100

    update make_dataset.py and src_demo

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,15 +3,19 @@ from typing import Tuple
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from src.entities.split_params import SplittingParams # adnan adjusted
+from src.entities.split_params import SplittingParams
 
 
 def split_train_val_data(
         data: pd.DataFrame,
+        target_col: str,
         params: SplittingParams
-) -> Tuple[pd.DataFrame, pd.DataFrame]:
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    
+    X = data.drop(target_col, axis=1)
+    y = data[target_col]
 
-    train_data, test_data = train_test_split(
-        data, test_size=params.val_size, random_state=params.random_state
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=params.val_size, random_state=params.random_state, stratify=y, shuffle=True
     )
-    return train_data, test_data
+    return X_train, X_test, y_train, y_test
