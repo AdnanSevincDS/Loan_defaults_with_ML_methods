7b9fa97ec16734d5886c70d22242ec8b9042bb1f
AdnanSevincDS <72599641+AdnanSevincDS@users.noreply.github.com>
2024-03-20 13:46:02 +0100

    updated
    Description added and try except block removed

--- src/tests/test_make_dataset.py
+++ src/tests/test_make_dataset.py
@@ -7,27 +7,19 @@ from src.entities.split_params import SplittingParams
 
 
 def test_split_train_val_data(data:pd.DataFrame, target_col:str) -> NoReturn:
-
+    """
+    Test the function that splits the data into training and validation data
+    """
     test_size = 0.3
     params = SplittingParams(val_size=test_size, random_state=42)
     X_train, X_test, y_train, y_test = split_train_val_data(data, target_col, params)
 
     tolerance = 0.0001
 
-    try:
-        assert len(X_train) >= len(data) * (0.7 - tolerance)
-        assert len(X_test) <= len(data) * (0.3 + tolerance)
-
-        assert len(X_train) + len(X_test) == len(data)
-
-        assert isinstance(X_train, pd.DataFrame)
-        assert isinstance(X_test, pd.DataFrame)
-        assert isinstance(y_train, pd.Series)
-        assert isinstance(y_test, pd.Series)
-    except AssertionError:
-        print("The split has not been performed correctly.")
-        raise
-    else:
-        print("The split has been performed correctly.")
-
-
+    assert len(X_train) >= len(data) * (0.7 - tolerance)
+    assert len(X_test) <= len(data) * (0.3 + tolerance)
+    assert len(X_train) + len(X_test) == len(data)
+    assert isinstance(X_train, pd.DataFrame)
+    assert isinstance(X_test, pd.DataFrame)
+    assert isinstance(y_train, pd.Series)
+    assert isinstance(y_test, pd.Series)
